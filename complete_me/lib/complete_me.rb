a4dc9bcedc78997498e3ec1befbc2a1fdd57f458
John Livingston <jclivingston316@gmail.com>
2018-03-24 14:37:37 -0600

    adds spec harness, changes class name to complete_me

---
+++ complete_me/lib/complete_me.rb
@@ -0,0 +1,2 @@
+class CompleteMe
+end
---
+++ complete_me/test/complete_me_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/complete_me'
+
+class CompleteMeTest < MiniTest::Test
+
+  def setup
+    @complete_me = CompleteMe.new
+  end
+
+  def test_it_exists
+    assert_instance_of CompleteMe, @complete_me
+  end
+
+end
