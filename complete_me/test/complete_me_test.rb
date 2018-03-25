cf44ba4394ffe62e5f71de81b75aa0ae7e06451a
Jude Lawson <lawsonjdutton@gmail.com>
2018-03-25 08:37:47 -0600

    Adds children node types test for complete_me

--- complete_me/test/complete_me_test.rb
+++ complete_me/test/complete_me_test.rb
@@ -13,4 +13,15 @@ class CompleteMeTest < MiniTest::Test
     assert_instance_of CompleteMe, @complete_me
   end
 
+  def test_node_can_be_inserted
+    @complete_me.insert("hello")
+    assert_instance_of Node, @complete_me.root_node
+  end
+
+  def test_child_node_has_correct_children_types
+    @complete_me.insert("hi")
+    assert_instance_of Node, @complete_me.root_node.children["h"]
+    assert_instance_of Node, @complete_me.root_node.children["h"].children["i"]
+  end
+
 end
