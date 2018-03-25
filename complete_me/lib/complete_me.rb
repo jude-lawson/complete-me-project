13171a4a91daddb1222c08c0ba444b5f9ae45def
John Livingston <jclivingston316@gmail.com>
2018-03-24 18:17:46 -0600

    renames trie to complete_me and deletes trie_test

--- complete_me/lib/complete_me.rb
+++ complete_me/lib/complete_me.rb
@@ -4,6 +4,22 @@ require 'pry'
 
 class CompleteMe
 
+  attr_accessor :root_node
 
+  def initialize
+    @root_node = Node.new
+  end
+
+  # insert
+  def insert(word)
+    node = @root_node
+    word.chars.map do |letter|
+      if !node.children.has_key?(letter)
+        node.children[letter] = Node.new
+      end
+      node = node.children[letter]
+    end
+    node.flag = true
+  end
 
 end
