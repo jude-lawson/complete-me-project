cf7cde621df093c9fe439f9f83bf605360b29277
Jude Lawson <lawsonjdutton@gmail.com>
2018-03-27 14:31:53 -0600

    It's no stylish, but adds the frontend UI

---
+++ complete_me/frontend/complete_me_app.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+require_relative '../lib/complete_me'
+
+get '/' do
+  erb :index
+end
+
+post '/' do
+  cm = CompleteMe.new
+  dictionary = File.read("/usr/share/dict/words")
+  cm.populate(dictionary)
+  suggestions = cm.suggest(params[:word])
+  erb :new_index, :locals => {:suggestions => suggestions}
+end
